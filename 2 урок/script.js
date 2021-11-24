61cf8a294385d838995ba8e4e0639afa7c658b97
vladimirfilippov <91004492+szkogkp@users.noreply.github.com>
2021-11-25 01:24:48 +0500

    2 hw
    2 hw

--- 2 урок/script.js
+++ 2 урок/script.js
@@ -1,65 +1,138 @@
-// const goods = [
-//     { title: 'Shirt', price: 150 },
-//     { title: 'Socks', price: 50 },
-//     { title: 'Jacket', price: 350 },
-//     { title: 'Shoes', price: 250 },
-// ];
-
-// const $goodsList = document.querySelector('.goods-list');
-  
-// const renderGoodsItem = ({ title, price }) => {
-//     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-// };
-  
-// const renderGoodsList = (list = goods) => {
-//     let goodsList = list.map(
-//             (item) =>  {
-//                 return renderGoodsItem(item)
-//             }
-//         ).join('');
-
-//     $goodsList.insertAdjacentHTML('beforeend', goodsList);
-// }
-  
-// renderGoodsList();
-
-
 class GoodsItem {
-    constructor(title, price) {
-      this.title = title;
-      this.price = price;
-    }
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
 
-    render() {
-      return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-    }
+  render() {
+    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+  }
 }
 
 class GoodsList {
-    constructor() {
-      this.goods = [];
-    }
+  constructor() {
+    this.goods = [];
+  }
 
-    fetchGoods() {
-        this.goods = [
-          { title: 'Shirt', price: 150 },
-          { title: 'Socks', price: 50 },
-          { title: 'Jacket', price: 350 },
-          { title: 'Shoes', price: 250 },
-        ];
-    }
+  fetchGoods() {
+    this.goods = [
+      { title: 'Shirt', price: 150 },
+      { title: 'Socks', price: 50 },
+      { title: 'Jacket', price: 350 },
+      { title: 'Shoes', price: 250 },
+    ];
+  }
 
-    render() {
-        let listHtml = '';
-        this.goods.forEach(good => {
-          const goodItem = new GoodsItem(good.title, good.price);
-          listHtml += goodItem.render();
-        });
-        document.querySelector('.goods-list').innerHTML = listHtml;
+  render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+
+  getTotalCost() {
+    let totalCost = 0;
+    for (let i = 0; i < this.goods.length; i++) {
+      totalCost += this.goods[i].price;
     }
-    
-}  
+    return totalCost;
+  }
+
+}
+
+class Cart {
+  constructor() {
+
+  }
+  clear() { }
+}
+
+class CartEl {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+
+  remove() { }
+  add() { }
+}
+
 
 const list = new GoodsList();
 list.fetchGoods();
 list.render();
+
+// 3. *Некая сеть фастфуда предлагает несколько видов гамбургеров:
+// ### Маленький (50 рублей, 20 калорий).
+// ### Большой (100 рублей, 40 калорий). ### Гамбургер может быть с одним из нескольких видов начинок (обязательно):
+// ### С сыром (+10 рублей, +20 калорий).
+// ### С салатом (+20 рублей, +5 калорий).
+// ### С картофелем (+15 рублей, +10 калорий). ### Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий) и полить майонезом (+20 рублей, +5 калорий). ### 3Напишите программу, рассчитывающую стоимость и калорийность гамбургера. Можно использовать примерную архитектуру класса из методички, но можно использовать и свою.
+
+class Hamburger {
+  constructor(base, filling) {
+    this.base = base;
+    this.filling = filling;
+  }
+
+  addSeasoning() {
+    this.seasoning = true;
+  }
+
+  addMayonnaise() {
+    this.mayonnaise = true;
+  }
+
+  getQuantity() {
+    let calories = 0;
+    let price = 0;
+
+    switch (this.base) {
+      case 'Little':
+        price += 50;
+        calories += 20;
+        break;
+      case 'Big':
+        price += 100;
+        calories += 40;
+        break;
+      default:
+        console.log('Неверная база');
+        break;
+    }
+
+    switch (this.filling) {
+      case 'Cheese':
+        price += 10;
+        calories += 20;
+        break;
+      case 'Salad':
+        price += 20;
+        calories += 5;
+        break;
+      case 'Potato':
+        price += 15;
+        calories += 10;
+        break;
+      default:
+        console.log('Неверная начинка');
+        break;
+    }
+
+    if (this.seasoning) {
+      price += 15;
+      calories += 0;
+    }
+
+    if (this.mayonnaise) {
+      price += 20;
+      calories += 5;
+    }
+
+    console.log(`Цена: ${price}\nКалорий: ${calories}`);
+  }
+}
+
+let cheezburger = new Hamburger('Big', 'Salad');
